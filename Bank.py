188d6c15fc66bc4cc54fafd59230dd740a4b8c5a
Sameed234 <sameedbondre@gmail.com>
2019-07-19 09:47:29 +0530

    Need help in how to connect to database i.e. sql, done with account and name validation

--- Bank.py
+++ Bank.py
@@ -1,28 +1,45 @@
 class Account :
     def createAccount(self):
-        self.accNo= int(input("Enter the account no : "))
+        
+        validAccNo=False
+        while not validAccNo:
+          accNo= input("Enter the account no : ")
+          if len(accNo)==7 and accNo.isdigit():    
+            validAccNo=True
+          else:
+            print("Account Number Must Contain 7 Digits only..")
+        print("Account Number : "+ accNo)   
+        
+
         self.name = input("Enter the account holder name : ")
+
+        for char in self.name:
+          if not char.isalpha():
+            print("Invalid name")
+            name = input("Enter the account holder name : ")
+
         self.type = input("Ente the type of account [Current/Saving] : ")
         self.deposit = float(input("Enter The Initial amount(>=500 for Saving and >=1000 for current"))
         print("\n\n\nAccount Created")
     
-        print("Account Number : ",self.accNo)
+       # print("Account Number : ",accNo)
         print("Account Holder Name : ", self.name)
         print("Type of Account",self.type)
         print("Balance : ",self.deposit)
 
     def depositamount(self): 
+        self.balance=self.deposit
         amount=float(input("Enter amount to be Deposited: ")) 
         self.balance += amount 
         print("\n Amount Deposited:",self.balance) 
   
     def withdraw(self): 
-        amount = float(input("Enter amount to be Withdrawn: ")) 
+        amount=float(input("Enter amount to be Withdrawn: ")) 
         if self.balance>=amount: 
-            self.balance-=amount 
-            print("\n You Withdrew:", amount) 
+          self.balance-=amount 
+          print("\n You Withdrew:",amount)   
         else: 
-            print("\n Insufficient balance  ") 
+           print("\n Insufficient balance  ") 
     
     def display(self): 
         print("\n Net Available Balance=",self.balance) 
@@ -33,7 +50,7 @@ obj_s.depositamount()
 obj_s.withdraw() 
 obj_s.display() 
 
-    	
+      
     
     
     
